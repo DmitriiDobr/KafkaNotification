d5ce2dbe2ea1999388b50f7796672819dcbbb716
DmitriiDobr <72938753+DmitriiDobr@users.noreply.github.com>
2023-06-19 23:40:36 +0300

    changes client

--- pkg/client.go
+++ pkg/client.go
@@ -1,7 +1,6 @@
 package kafkaNotification
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/segmentio/kafka-go"
@@ -16,15 +15,20 @@ type Config struct {
 	Topic   string
 }
 
-func New(ctx context.Context, cfg *Config) (*Client, error) {
-	conn, err := kafka.DialLeader(ctx, "tcp", cfg.Brokers, cfg.Topic, 1)
+func New(cfg *Config) (*Client, error) {
+	conn, err := kafka.Dial("tcp", cfg.Brokers)
+	if err != nil {
+		return nil, err
+	}
+	topicConfigs := kafka.TopicConfig{Topic: cfg.Topic, NumPartitions: 1, ReplicationFactor: 1}
+	err = conn.CreateTopics(topicConfigs)
 	if err != nil {
 		return nil, err
 	}
 	return &Client{connection: conn}, nil
 }
 
-func (c *Client) Notify(ctx context.Context, message Message, topic string) error {
+func (c *Client) Notify(message Message, topic string) error {
 	body, err := json.Marshal(message)
 	if err != nil {
 		return err
